6da5f183b6eb740c7d67d16f27bf2b0013f0631c
thiago.vidigal <thiago.vidigal@ueg.br>
2024-12-18 16:44:32 -0300

    add testes and refactoring

--- 03_CleanCode/a1_refac/example2/example2_my_after.js
+++ 03_CleanCode/a1_refac/example2/example2_my_after.js
@@ -1,40 +1,39 @@
+
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
+
 function validate(cpfRaw) {
-	if (!cpfRaw) return false;
-    if (cpfRaw.length >= 11 || cpfRaw.length <= 14){
-        cpf=cpfRaw
-            .replace('.','')
-            .replace('.','')
-            .replace('-','')
-            .replace(" ","");      
-        if (!cpf.split("").every(c => c === cpf[0])) {
-            try{  
-                let     d1, d2;  
-                let     dg1, dg2, rest;  
-                let     digito;  
-                    let     nDigResult;  
-                d1 = d2 = 0;  
-                dg1 = dg2 = rest = 0;                              
-                for (let nCount = 1; nCount < cpf.length -1; nCount++) {    
-                        digito = parseInt(cpf.substring(nCount -1, nCount));  							
-                        d1 = d1 + ( 11 - nCount ) * digito;                  
-                        d2 = d2 + ( 12 - nCount ) * digito;  
-                };                              
-                rest = (d1 % 11);                  
-                dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                d2 += 2 * dg1;  
-                rest = (d2 % 11);  
-                if (rest < 2)  
-                    dg2 = 0;  
-                else  
-                    dg2 = 11 - rest;                  
-                    let nDigVerific = cpf.substring(cpf.length-2, cpf.length);  
-                nDigResult = "" + dg1 + "" + dg2;  
-                return nDigVerific == nDigResult;
-            }catch (e){     
-                return false;  
-            }  
-        } else return false
+    if (!cpfRaw) return false;
+    cpf=clear(cpfRaw);
+    if (isValidLength(cpf)) return false;              
+    if (isBlocked(cpf)) return false;                                                                              
+    const digit1 = calculateDigit(cpf, FACTOR_DIGIT_1); 
+    const digit2 = calculateDigit(cpf, FACTOR_DIGIT_2);                    
+    const atualDigit = cpf.slice(9);  
+    const calculatedDigit = `${digit1}${digit2}`;  
+    return atualDigit == calculatedDigit;  
+}
+
+function clear(cpf) {
+    return cpf.replace(/[\.\-]*/g, "");
+}
+
+const isValidLength = (cpf) => {
+    return cpf.length !== 11;
+}
+
+const isBlocked = (cpf) => {
+    const [firstDigit] = cpf;
+    return [...cpf].every(digit => digit === firstDigit);
+}
+
+const calculateDigit = (cpf, factor) => {
+    let total = 0
+    for (const digit of cpf) {
+        if (factor > 1) total +=parseInt(digit) * factor --;
     }
+    const rest = total % 11
+    return rest < 2 ? 0 : 11 - rest;
 }
 
 module.exports = {
