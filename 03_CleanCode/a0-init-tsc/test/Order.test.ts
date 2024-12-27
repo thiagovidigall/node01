50d33b30b84cfa138c8df1636b4946a26710c837
thiago.vidigal <thiago.vidigal@uau.com.br>
2024-12-27 12:02:45 -0300

    error teste execution

--- 03_CleanCode/a0-init-tsc/test/Order.test.ts
+++ 03_CleanCode/a0-init-tsc/test/Order.test.ts
@@ -1,6 +1,7 @@
 import Order from "../src/Order";
 
 test("deve criar um pedido informando o cpf", function () {
-    const order = new Order('89145160163');
+    const cpf = "839.435.452-10";
+    const order = new Order(cpf);
     expect(order).toBeTruthy();
 });
